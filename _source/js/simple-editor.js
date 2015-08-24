57dc1578c15c45aa574d00696e52ce4ce40e24bd
Caio Reis <caio@cyclumsn.com>
2015-08-24 03:50:52 -0300

    Adding buttons and editor triggers

--- _source/js/simple-editor.js
+++ _source/js/simple-editor.js
@@ -1,1 +1,38 @@
-SimpleEditor = {};
+SimpleEditor = {
+  updateButtons: function () {
+    var bold = document.queryCommandState("Bold");
+    var italic = document.queryCommandState("Italic");
+    var underline = document.queryCommandState("Underline");
+
+    if (bold === true) {
+      $('[data-action=bold]').addClass('active');
+    } else {
+      $('[data-action=bold]').removeClass('active');
+    }
+
+    if (italic === true) {
+      $('[data-action=italic]').addClass('active');
+    } else {
+      $('[data-action=italic]').removeClass('active');
+    }
+
+    if (underline === true) {
+      $('[data-action=underline]').addClass('active');
+    } else {
+      $('[data-action=underline]').removeClass('active');
+    }
+  },
+  registerTriggers: function () {
+
+    $('.btn-action').on('click', function (e) {
+      var action = $(this).attr('data-action');
+      document.execCommand(action);
+      SimpleEditor.updateButtons();
+    });
+
+    $('#editor').on('click', function (e) {
+      SimpleEditor.updateButtons();
+    });
+
+  }
+};
