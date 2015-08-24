bb863e42f07a2a2bce99f92ea7f68cb1dd647a82
Caio Reis <caio@cyclumsn.com>
2015-08-24 03:39:27 -0300

    Creating grunt tasks and adding source files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,76 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    base_path: '',
+
+    build:     '_public',
+    css_build: '_public/css',
+    js_build:  '_public/js',
+
+    css_src:   '_source/css',
+    js_src:    '_source/js',
+
+    meta: {
+      css : {
+        banner:
+        '/*========================================================================================!\n' +
+        ' * app.css <%= pkg.name %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
+        ' * Author: <%= pkg.author %>\n' +
+        ' * E-mail: <%= pkg.email %>\n' +
+        ' * Site: <%= pkg.site %>\n' +
+        ' ========================================================================================*/'
+      },
+
+      js : {
+
+        banner:
+        '/*========================================================================================!\n' +
+        ' * app.js <%= pkg.name %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
+        ' * Author: <%= pkg.author %>\n' +
+        ' * E-mail: <%= pkg.email %>\n' +
+        ' * Site: <%= pkg.site %>\n' +
+        ' ========================================================================================*/'
+
+      }
+
+    },
+
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: '<%= meta.js.banner %>\n',
+        mangle: false
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        files: {
+          '<%= js_build %>/app.min.js': ['<%= js_build %>/app.js'],
+        },
+      },
+    },
+
+    concat: {
+      options:{
+        separator: ';'
+      },
+      basic_and_extras: {
+        files: {
+          '<%= js_build %>/app.js' : ['<%= js_src %>/*.js']
+        },
+      },
+    },
+
+    cssmin: {
+      options: {
+        banner: '<%= meta.css.banner %>\n',
+        keepSpecialComments: 0
+      },
+      compress: {
+        files: {
+          '<%= css_build %>/app.min.css': [ '<%= css_src %>/*.css' ],
+        }
       }
     },
+
     serve: {
       options: {
         port: 9000
@@ -21,9 +82,11 @@ module.exports = function(grunt) {
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks( 'grunt-contrib-concat');
+  grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
   grunt.loadNpmTasks('grunt-serve');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'serve']);
 
 };
